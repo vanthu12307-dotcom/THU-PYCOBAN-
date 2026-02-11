05d6ea3a7932c6243a25a34a316a49217e2c708b
vanthu12307-dotcom <vanthu12307@gmail.com>
2026-02-12 02:01:27 +0700

    Create bai_1.py

---
+++ ss3/bai_tap/bai_1.py
@@ -0,0 +1,9 @@
+name = "Thư"
+age = "18"
+hobby = "xem phim"
+favorite_subject = "biology"
+print( "Tên: " + name )
+print ("Tuổi: " + age )
+print ("Sở thích: " + hobby )
+print ("Môn học yêu thích: " + favorite_subject )
+
